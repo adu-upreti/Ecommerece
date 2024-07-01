e1128a35fbd693f207ec0e93d096923c2d0ec86f
adu-upreti <162395567+adu-upreti@users.noreply.github.com>
2024-07-01 14:32:42 +0545

    form fields integration in templates

--- Ecommerce1/product/forms.py
+++ Ecommerce1/product/forms.py
@@ -2,14 +2,21 @@ from django import forms
 from .models import Products,Category
 
 
-class product_form(forms.ModelForm):
-        class Meta:
-         model = Products
-         fields = "__all__"
-
-
-
 class category_form(forms.ModelForm):
         class Meta:
          model = Category
          fields = "__all__"
+
+class product_form(forms.ModelForm):
+        class Meta:
+         model = Products
+         fields = "__all__"
+         widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control validate', 'required': 'required'}),
+            'description': forms.Textarea(attrs={'class': 'form-control validate', 'rows': 3, 'required': 'required'}),
+            'category': forms.Select(attrs={'class': 'custom-select tm-select-accounts'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control validate', 'data-large-mode': 'true'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control validate', 'required': 'required'}),
+        }
+
+
